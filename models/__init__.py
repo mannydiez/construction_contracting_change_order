9cc239aecb9f89ca502f9ec5e578d97f68ff1235
diez43011 <diez43011@gmail.com>
2019-04-10 13:38:46 +0800

    Initial commit

---
+++ models/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+import construction_change_order
+import construction_change_order_line
+import project
+import project_task
+import res_partner
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
